dcc765c51c1fdb109370db56a944bbe880ec47d7
jeremyalv <jeremyalvax@gmail.com>
2024-04-29 12:35:45 +0700

    feat: add MutationObserver and changed listener to window.onload event

--- src/pages/Content/index.ts
+++ src/pages/Content/index.ts
@@ -1,17 +1,20 @@
 console.log('Content script works!');
 console.log('Must reload extension for modifications to take effect.');
 
-// Run Content Script on DOM Load
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("DOMContentLoaded");
+// Run Content Script on Window Load
+
+// document.addEventListener("load", () => {
+window.addEventListener("load", () => {
+  console.log("window.onLoad()...");
   
   chrome.runtime.sendMessage("VALIDATE_URL", (response: any) => {
     if (response === "VALIDATE_URL_SUCCESS") {
       console.log("VALIDATE_URL_SUCCESS, running script...");
       runContentScript();
+    } else {
+      console.log("INVALID_URL");
     }
     
-    console.log("INVALID_URL");
     return;
   });
 });
@@ -52,11 +55,12 @@ const findTermsURLInPage = (): string | null => {
   
   const anchorTagsList = document.querySelectorAll("a");
 
-  console.log(`document: ${document}`);
-  console.log(`anchorTagsList: ${anchorTagsList}`);
-  
+  const fakeDOM = document.getElementsByTagName('body')[0];
+  console.log(`DOM length ${fakeDOM.innerHTML.length} chars\n`);
+  console.log("DOM\n", fakeDOM);
+
   if (anchorTagsList.length == 0) {
-    console.log(`No links found: anchorTagsList has 0 elements!`);
+    console.log(`ERROR, No links found: anchorTagsList has 0 elements!`);
     return null;
   }
 
@@ -74,10 +78,47 @@ const findTermsURLInPage = (): string | null => {
 };
 
 const runContentScript = async () => {
+  useMutationObserver();
+
   let termsURL = findTermsURLInPage();
 
   // TODO pass to backend
   console.log("termsURL:", termsURL);
 
   return true;
+};
+
+// MutationObserver impl
+const initMutationObserver = () => {
+  const callback = (mutationList: any, observer: any) => {
+    for (const mutation of mutationList) {
+      if (mutation.type === "childList") {
+        console.log("A child node has been added or removed.");
+      } else if (mutation.type === "attributes") {
+        console.log(`The ${mutation.attributeName} attribute was modified.`);
+      }
+    }
+  };
+
+  return new MutationObserver(callback);
+};
+
+const useMutationObserver = () => {
+  const DURATION_SECS = 30;
+
+  const targetNode = document.querySelector("body") as Node;
+
+  const config = {
+    attributes: true,
+    childList: true,
+    subtree: true,
+  }
+
+  const observer = initMutationObserver();
+  
+  observer.observe(targetNode, config);
+  
+  setTimeout(() => {
+    observer.disconnect();
+  }, DURATION_SECS*1000);
 };
