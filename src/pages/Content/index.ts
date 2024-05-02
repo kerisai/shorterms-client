b1e1e0b139184921101b6be0d3e90964bcfc7bce
Nayyara Airlangga <nayyara.airlangga@gmail.com>
2024-05-02 23:43:16 +0700

    feat: automatically find tos link on page load

--- src/pages/Content/index.ts
+++ src/pages/Content/index.ts
@@ -1,31 +1,57 @@
 console.log('Content script works!');
 console.log('Must reload extension for modifications to take effect.');
 
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  console.log("Inside FIND_TOS_LINK_REQUEST");
+chrome.runtime.sendMessage('VALIDATE_URL', async (response: any) => {
+  if (response === 'VALIDATE_URL_SUCCESS') {
+    console.log('VALIDATE_URL_SUCCESS, running script...');
 
-  if (message === "FIND_TOS_LINK_REQUEST") {
-    chrome.runtime.sendMessage("VALIDATE_URL", async (response: any) => {
-      if (response === "VALIDATE_URL_SUCCESS") {
-        console.log("VALIDATE_URL_SUCCESS, running script...");
-  
+    try {
+      const termsURL = await runContentScript();
+
+      if (termsURL === null) {
+        throw new Error('Error: termsURL is null!');
+      }
+
+      console.log('SET termsURL', termsURL);
+      console.log(`sendResponse: ${termsURL}`);
+      console.log(termsURL);
+
+      chrome.storage.session.set({ termsURL: termsURL });
+    } catch (err) {
+      const error = err as unknown as Error;
+
+      console.log(error.message);
+    }
+  } else {
+    console.log('INVALID_URL');
+  }
+});
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  console.log('Inside FIND_TOS_LINK_REQUEST');
+
+  if (message === 'FIND_TOS_LINK_REQUEST') {
+    chrome.runtime.sendMessage('VALIDATE_URL', async (response: any) => {
+      if (response === 'VALIDATE_URL_SUCCESS') {
+        console.log('VALIDATE_URL_SUCCESS, running script...');
+
         try {
           const termsURL = await runContentScript();
-          
+
           if (termsURL === null) {
-            throw new Error("Error: termsURL is null!");
+            throw new Error('Error: termsURL is null!');
           }
-  
-          console.log("SET termsURL", termsURL);
+
+          console.log('SET termsURL', termsURL);
           console.log(`sendResponse: ${termsURL}`);
           sendResponse(termsURL);
         } catch (err) {
           const error = err as unknown as Error;
-          
+
           console.log(error.message);
         }
       } else {
-        console.log("INVALID_URL");
+        console.log('INVALID_URL');
       }
     });
   }
@@ -38,17 +64,17 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 const filterAnchorsByTextContent = (
   anchorTagsList: NodeListOf<HTMLAnchorElement>
 ) => {
-  let termsURL = "";
+  let termsURL = '';
 
   enum AnchorTextKeywords {
-    TERMS = "terms",
-    TOS = "tos",
+    TERMS = 'terms',
+    TOS = 'tos',
   }
 
   for (let i = 0; i < anchorTagsList.length; i++) {
     const item = anchorTagsList[i];
     const itemLower = item.text.toLowerCase();
-    
+
     if (itemLower.includes(AnchorTextKeywords.TERMS)) {
       termsURL = item.href;
       break;
@@ -65,29 +91,31 @@ const filterAnchorsByTextContent = (
 
 const findTermsURLInPage = async (): Promise<string> => {
   const POLLING_DURATION_SEC = 10;
-  
-  let termsURL = "";
-  
+
+  let termsURL = '';
+
   try {
     const anchorTagsList = await pollFindTermsURL(POLLING_DURATION_SEC);
 
     const fakeDOM = document.getElementsByTagName('body')[0];
     console.log(`DOM length ${fakeDOM.innerHTML.length} chars\n`);
-    console.log("DOM\n", fakeDOM);
+    console.log('DOM\n', fakeDOM);
 
     if (anchorTagsList.length == 0) {
-      throw new Error("Error: No links found, anchorTagsList has 0 elements!");
+      throw new Error('Error: No links found, anchorTagsList has 0 elements!');
     }
 
     // Filter by "Terms" related textContent
-    console.log("<a> tags\n", anchorTagsList);
-    console.log(`Running filterAnchorsByTextContent on ${anchorTagsList.length} <a> tags`);
+    console.log('<a> tags\n', anchorTagsList);
+    console.log(
+      `Running filterAnchorsByTextContent on ${anchorTagsList.length} <a> tags`
+    );
     termsURL = filterAnchorsByTextContent(anchorTagsList);
 
     if (termsURL.length === 0) {
-      throw new Error("Error: termsURL has length 0!");
+      throw new Error('Error: termsURL has length 0!');
     }
-    
+
     return termsURL;
   } catch (error) {
     // Throw error to caller
@@ -97,7 +125,7 @@ const findTermsURLInPage = async (): Promise<string> => {
 
 // Polling process for finding Terms URL for pages with delayed script load
 const pollFindTermsURL = async (
-  duration_secs: number,
+  duration_secs: number
 ): Promise<NodeListOf<HTMLAnchorElement>> => {
   const MS_IN_SECONDS = 1000;
   const POLL_COUNT = 20;
@@ -109,18 +137,24 @@ const pollFindTermsURL = async (
   return new Promise((resolve, reject) => {
     const timer = setInterval(() => {
       if (pollingTries >= POLL_COUNT) {
-        console.log("Max poll tries reached...");
+        console.log('Max poll tries reached...');
         clearTimeout(timer);
-        reject(new Error("Error: Failed to retrieve anchor element within specified duration"));
+        reject(
+          new Error(
+            'Error: Failed to retrieve anchor element within specified duration'
+          )
+        );
       }
-      
+
       pollingTries++;
       console.log(`Running poll... #${pollingTries}`);
 
-      anchorTagsList = document.querySelectorAll("a");
-  
+      anchorTagsList = document.querySelectorAll('a');
+
       if (anchorTagsList.length === 0) {
-        console.log(`Poll ${pollingTries} - No links found: anchorTagsList has 0 elements!`);
+        console.log(
+          `Poll ${pollingTries} - No links found: anchorTagsList has 0 elements!`
+        );
       } else {
         // Stop interval if anchor tags are found
         console.log(`Poll ${pollingTries} - Found result!`);
@@ -135,9 +169,9 @@ const pollFindTermsURL = async (
 const initMutationObserver = () => {
   const callback = (mutationList: any, observer: any) => {
     for (const mutation of mutationList) {
-      if (mutation.type === "childList") {
-        console.log("A child node has been added or removed.");
-      } else if (mutation.type === "attributes") {
+      if (mutation.type === 'childList') {
+        console.log('A child node has been added or removed.');
+      } else if (mutation.type === 'attributes') {
         console.log(`The ${mutation.attributeName} attribute was modified.`);
       }
     }
@@ -149,21 +183,21 @@ const initMutationObserver = () => {
 const useMutationObserver = () => {
   const DURATION_SECS = 30;
 
-  const targetNode = document.querySelector("body") as Node;
+  const targetNode = document.querySelector('body') as Node;
 
   const config = {
     attributes: true,
     childList: true,
     subtree: true,
-  }
+  };
 
   const observer = initMutationObserver();
-  
+
   observer.observe(targetNode, config);
-  
+
   setTimeout(() => {
     observer.disconnect();
-  }, DURATION_SECS*1000);
+  }, DURATION_SECS * 1000);
 };
 
 /**
@@ -179,4 +213,3 @@ const runContentScript = async (): Promise<string> => {
     throw err;
   }
 };
-
